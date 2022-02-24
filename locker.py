cf6e7eaa0a369a25bed2942970dba2f7ab11134c
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-24 13:16:30 +0300

    this commit defines the deletion and creation of user

--- locker.py
+++ locker.py
@@ -1,7 +1,31 @@
-class User:
+class User: 
+# class user this is an instance of the user class
     '''
-    This class generates instances for creating an acoount
+    This class generates instances for creating an user
     '''
     user_list=[]
 
-    
+    def __init__(self,username,password):
+        '''
+        this functon define the properties for creation of account
+        
+        Args:
+            username: username
+            password: password
+        '''
+        self.username = username
+        self.password = password
+
+    def save_user(self):
+        '''
+        Test to save new user
+        '''
+        User.user_list.apend(self)
+
+    def delete_user(self):
+        '''
+        test to delete user
+        '''
+
+
+
