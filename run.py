f9c83e8f1d2b524aa099b273cf7c399708d1e72a
Hillary-Kechem <hillarykechem92@gmail.com>
2022-02-24 19:46:51 +0300

    save user function

--- run.py
+++ run.py
@@ -4,7 +4,15 @@ from locker import User
 
 def create_user(username, password):
     ''' 
-    Functiion to create a new user
+        Functiion to create a new user
     '''
     new_user=User(username, password)
     return new_user
+
+def save_user(user):
+    '''
+        save user data
+    '''
+    user.save_user()
+
+    
